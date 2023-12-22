e65d85500aefa385ffd05b0f4eb166e879fcf452
GVladislav <grishinvladislav97@gmail.com>
2023-12-22 14:12:16 +0300

    Finished parser

--- main.py
+++ main.py
@@ -2,33 +2,121 @@ import time
 import asyncio
 
 from aiohttp import ClientSession
+from more_itertools import chunked
 
 from tools.timer import async_timer
+from models import init_db, Session, Character
 
-BASE_URL = 'https://pokeapi.co/api/v2/pokemon'
-RATE = {'requests': 3, 'seconds': 1}
+BASE_URL = 'https://swapi.dev/api/people'
+RATE = {'requests': 10, 'seconds': 1, 'chunk_size': 20}
 
 
-async def get_pokemon(session, pokemon_id, semaphore):
+async def prepare_character_objects(data: list[dict[str, str]]) \
+        -> list[Character]:
+    char_objects = [Character(
+        person_id=int(char['url'].split('/')[-2]),
+        birth_year=char['birth_year'],
+        eye_color=char['eye_color'],
+        films=', '.join(char['films']),
+        gender=char['gender'],
+        hair_color=char['hair_color'],
+        height=char['height'],
+        home_world=char['homeworld'],
+        mass=char['mass'],
+        name=char['name'],
+        skin_color=char['skin_color'],
+        species=', '.join(char['species']),
+        starships=', '.join(char['starships']),
+        vehicles=', '.join(char['vehicles']),
+    ) for char in data]
+    return char_objects
+
+
+async def insert_to_db(characters: list[Character]) -> None:
+    print('starting to insert characters', flush=True)
+    async with Session() as session:
+        session.add_all(characters)
+        await session.commit()
+        print('finished inserting characters', flush=True)
+
+
+async def fetch_data(session, semaphore, url) \
+        -> dict[str, str | dict[str, str]]:
     async with semaphore:
         start = time.time()
-        print(f'fetching pokemon {pokemon_id}', flush=True)
-        async with session.get(f'{BASE_URL}/{pokemon_id}') as response:
-            pokemon = await response.json()
+        resource_type, idx, _ = url.split('/')[-3:]
+        print(f'fetching {resource_type} #{idx}', flush=True)
+        async with session.get(url) as response:
+            json_data = await response.json()
+            print(f'fetched {resource_type} #{idx}', flush=True)
             time_to_fetch = time.time() - start
             if time_to_fetch < RATE['seconds']:
+                print(f'sleeping for {RATE["seconds"] - time_to_fetch} '
+                      f'seconds', flush=True)
                 await asyncio.sleep(RATE['seconds'] - time_to_fetch)
-            return pokemon['id'], pokemon['name']
+            return {'status_code': response.status, 'data': json_data}
 
 
 @async_timer
-async def main():
+async def main() -> None:
     semaphore = asyncio.Semaphore(RATE['requests'])
     async with ClientSession() as session:
-        tasks = [get_pokemon(session, i, semaphore) for i in range(1, 21)]
-        pokemons = await asyncio.gather(*tasks)
-        for pokemon in pokemons:
-            print(f'#{pokemon[0]} - {pokemon[1]}', flush=True)
+
+        urls_with_values = {}
+
+        for chunk_of_ids in chunked(range(1, 100), RATE['chunk_size']):
+            char_requests = await asyncio.gather(
+                *[fetch_data(session, semaphore, f'{BASE_URL}/{i}/')
+                  for i in chunk_of_ids]
+            )
+
+            chars = [char['data'] for char in char_requests if char['status_code'] == 200]
+
+            linked_fields = {
+                'films': 'title',
+                'species': 'name',
+                'homeworld': 'name',
+                'starships': 'name',
+                'vehicles': 'name'
+            }
+
+            for char in chars:
+                for field in linked_fields.keys():
+                    if field == 'homeworld':
+                        if char[field] not in urls_with_values.keys():
+                            retrieve_task = asyncio.create_task(
+                                fetch_data(session, semaphore, char[field])
+                            )
+                            urls_with_values[char[field]] = retrieve_task
+                        continue
+
+                    for url in char[field]:
+                        if url not in urls_with_values.keys():
+                            retrieve_task = asyncio.create_task(
+                                fetch_data(session, semaphore, url)
+                            )
+                            urls_with_values[url] = retrieve_task
+
+            await asyncio.gather(*urls_with_values.values())
+
+            for char in chars:
+                for field in linked_fields.keys():
+                    if field == 'homeworld':
+                        char[field] \
+                            = urls_with_values[char[field]].result()['data'][linked_fields[field]]
+                        continue
+                    for idx, url in enumerate(char[field]):
+                        char[field][idx] \
+                            = urls_with_values[url].result()['data'][linked_fields[field]]
+
+            char_objects = await prepare_character_objects(chars)
+            asyncio.create_task(insert_to_db(char_objects))
+        await asyncio.gather(*asyncio.all_tasks() - {asyncio.current_task()})
 
 
-asyncio.run(main())
+async def entry_point() -> None:
+    await init_db()
+    await main()
+
+if __name__ == '__main__':
+    asyncio.run(entry_point())
--- models.py
+++ models.py
@@ -1,7 +1,47 @@
-PG_USER = 'api'
-PG_PASSWORD = 'secret'
-PG_HOST = 'localhost'
-PG_PORT = '5431'
-PG_NAME = 'pokemon'
+import os
+
+from sqlalchemy import Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, \
+    AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+PG_USER = os.getenv('PG_USER', 'api')
+PG_PASSWORD = os.getenv('PG_PASSWORD', 'secret')
+PG_HOST = os.getenv('PG_HOST', 'localhost')
+PG_PORT = os.getenv('PG_PORT', '5431')
+PG_NAME = os.getenv('PG_NAME', 'swapi')
 
 PG_DSN = f'postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_NAME}'
+
+engine = create_async_engine(PG_DSN)
+Session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    person_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(100), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    films: Mapped[str] = mapped_column(String(200), nullable=False)  # list
+    gender: Mapped[str] = mapped_column(String(100), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    height: Mapped[str] = mapped_column(String(100), nullable=False)
+    home_world: Mapped[str] = mapped_column(String(100), nullable=False)  # link
+    mass: Mapped[str] = mapped_column(String(100), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    species: Mapped[str] = mapped_column(String(200), nullable=False)  # list
+    starships: Mapped[str] = mapped_column(String(200), nullable=False)  # list
+    vehicles: Mapped[str] = mapped_column(String(200), nullable=False)  # list
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
